c12ebeda181b069567cd869068f487af75f06073
Garsaa <gabriel.garsa7@gmail.com>
2022-11-23 12:13:58 -0300

    MOVE FUNCIONANDO 100%

--- src/k_and_s_pkg.sv
+++ src/k_and_s_pkg.sv
@@ -3,11 +3,11 @@ package k_and_s_pkg;
     I_NOP,//ok
     I_LOAD,//ok
     I_STORE,//ok
-    I_MOVE,
-    I_ADD,
-    I_SUB,
-    I_AND,
-    I_OR,
+    I_MOVE,//ok
+    I_ADD,//ok
+    I_SUB,//ok
+    I_AND,//ok
+    I_OR,//ok
     I_BRANCH,
     I_BZERO,
     I_BNZERO,
